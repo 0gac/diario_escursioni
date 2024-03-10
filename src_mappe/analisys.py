2669bc30d7b384e8dcbd66f41d52c11dd8c8104f
Giacomo Marcon <marcongiacomo@gmail.com>
2024-03-10 11:21:52 +0100

    functionality to produce a track for multiday trekkings, code refactoring

--- src_mappe/analisys.py
+++ src_mappe/analisys.py
@@ -1,146 +1,128 @@
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 import numpy as np
-import re
 import sys
 import datetime
+import tools as tl
 
 
-class GpxReadout:
-    coordinate = []
-    times = []
-    hr = []
-    ele = []
-    numpoints = 0
-    numhr = 0
-    numtime = 0
-    numele = 0
-
-
-    def __init__(self, filepath: str):
-    # readout of the gpx file
-        with open(filepath, "r") as file:
-            intopoints = False
-            for line in file:
-                if not intopoints:
-                    if re.match("\s*<trk>\s*", line) is not None:
-                        intopoints = True
-                else:
-                    testpoint = re.match("\s*<trkpt\slat=\"([0-9]{2}.[0-9]*)\"\slon=\"([0-9]{2}.[0-9]*)\">\s*",
-                                         line)
-                    if testpoint is not None:
-                        self.numpoints += 1
-                        self.coordinate.append([float(testpoint.group(1)), float(testpoint.group(2))])
-                        if self.numpoints % 1000 == 0:
-                            sys.stdout.write(str(self.numpoints) + "\r")
-                            sys.stdout.flush()
-                    else:
-                        testtime = re.match("\s*<time>[0-9]{4}-[0-9]{2}-[0-9]{2}T([0-9]{2}\:[0-9]{2}:[0-9]{2})\.000Z</time>\s*",
-                                            line)
-                        if testtime is not None:
-                            self.numtime += 1
-                            if self.numtime != self.numpoints:
-                                for i in range(self.numpoints - self.numtime):
-                                    self.times.append(datetime.time.fromisoformat('00:00:00'))
-                                self.numtime = self.numpoints
-                            else:
-                                pass
-                            time_obj = datetime.time.fromisoformat(testtime.group(1))
-                            self.times.append(time_obj)
-                        else:
-                            testhr = re.match("\s*<ns3:hr>([0-9]+)</ns3:hr>\s*", line)
-                            if testhr is not None:
-                                self.numhr += 1
-                                if self.numhr != self.numpoints:
-                                    for i in range(self.numpoints - self.numhr):
-                                        self.hr.append(np.nan)
-                                    self.numhr = self.numpoints
-                                else:
-                                    pass
-                                self.hr.append(int(testhr.group(1)))
-                            else:
-                                testele = re.match("\s*<ele>([0-9]+)\.[0-9]+</ele>\s*", line)
-                                if testele is not None:
-                                    self.numele += 1
-                                    if self.numele != self.numpoints:
-                                        for i in range(self.numpoints - self.numele):
-                                            self.ele.append(None)
-                                        self.numele = self.numpoints
-                                    else:
-                                        pass
-                                    self.ele.append(int(testele.group(1)))
-                                else:
-                                    pass
-
-        self.coordinate = np.array(self.coordinate)
-        self.coordinate[:, 0] = self.coordinate[:, 0] / np.max(self.coordinate[:, 0])
-        self.coordinate[:, 1] = self.coordinate[:, 1] / np.max(self.coordinate[:, 1])
-        self.times = np.array(self.times)
-
-        def tosec(x):
-                return (x.hour * 60 + x.minute) * 60 + x.second
-
-        tosec_vec = np.vectorize(tosec)
-
-        self.elap_time = tosec_vec(self.times) - tosec(min(self.times))
-        self.hr = np.array(self.hr)
-        self.ele = np.array(self.ele)
-
-    
-    def convert_elap(self, timesec):
-        eth = np.floor(timesec / 3600).astype(int)
-        etm = np.floor((timesec - 3600 * eth) / 60).astype(int)
-        ets = (timesec - 3600 * eth - 60 * etm).astype(int)
-        return str(eth) + ":" + str(etm) + ":" + str(ets)
-
-    
-
-
-def plottrack(gpx: GpxReadout, timeinput: np.ndarray, outpath: str):
+def plottrack(gpx: tl.GpxReadout, outpath: str, timeinput: np.ndarray = None):
     fig, ax = plt.subplots()
     if gpx.numele != 0:
         plot = ax.scatter(gpx.coordinate[:, 1], gpx.coordinate[:, 0], s=5, c=gpx.ele, cmap='viridis_r')
-        handles, labels = plot.legend_elements(prop="colors", alpha = 0.6)
-        ax.legend(handles, labels, loc = "lower left", title = "altitudine")
+        handles, labels = plot.legend_elements(prop="colors", alpha=0.6)
+        ax.legend(handles, labels, loc="lower left", title="altitudine")
     else:
         ax.scatter(gpx.coordinate[:, 1], gpx.coordinate[:, 0], s=5)
 
     ax.tick_params(
-        axis = "both",
-        which = "both",
-        bottom = False,
-        left = False,
-        labelbottom = False,
-        labelleft = False
+        axis="both",
+        which="both",
+        bottom=False,
+        left=False,
+        labelbottom=False,
+        labelleft=False
     )
     startpoint = np.reshape(gpx.coordinate[gpx.times == min(gpx.times)], 2)
     endpoint = np.reshape(gpx.coordinate[gpx.times == max(gpx.times)], 2)
     ax.scatter(startpoint[1], startpoint[0], s=60, color='green')
     ax.scatter(endpoint[1], endpoint[0], s=60, color='red')
-
-    timeinput = np.array([datetime.time.fromisoformat(x) for x in timeinput])
+    
 
     count_images = 0
+    if timeinput is not None:
+        timeinput = np.array([datetime.time.fromisoformat(x) for x in timeinput])
 
-    for ts in timeinput:
-        ubtime = datetime.time(hour=ts.hour, minute=ts.minute, second=59)
-        lbtime = datetime.time(hour=ts.hour, minute=ts.minute, second=0)
-        coord_time = gpx.coordinate[np.logical_and(gpx.times > lbtime, gpx.times < ubtime)]
-        pointpos = np.average(coord_time[:, 0:2], axis=0)
+        for ts in timeinput:
+            ubtime = datetime.time(hour=ts.hour, minute=ts.minute, second=59)
+            lbtime = datetime.time(hour=ts.hour, minute=ts.minute, second=0)
+            coord_time = gpx.coordinate[np.logical_and(gpx.times > lbtime, gpx.times < ubtime)]
+            pointpos = np.average(coord_time[:, 0:2], axis=0)
 
-        count_images += 1
-        ax.annotate(str(count_images), xy=(pointpos[1], pointpos[0]), xytext=(30, 30),
-                    textcoords='offset points',
-                    color='k', size='large',
-                    arrowprops=dict(
-                    arrowstyle='simple,tail_width=0.2,head_width=0.8,head_length=0.8',
-                    color='k'))
-    fig.savefig(outpath + 'track.pdf', bbox_inches = "tight")
+            count_images += 1
+            ax.annotate(str(count_images), xy=(pointpos[1], pointpos[0]), xytext=(30, 30),
+                        textcoords='offset points',
+                        color='k', size='large',
+                        arrowprops=dict(
+                        arrowstyle='simple,tail_width=0.2,head_width=0.8,head_length=0.8',
+                        color='k'))
+    fig.savefig(outpath + 'track.pdf', bbox_inches="tight")
     plt.close(fig)
     return count_images
 
 
-def plothr(gpx: GpxReadout, outpath: str):
+def plotmultiday(gpxs: list, outpath: str):
+    tracks = []
+    tracks_ele = []
+    tracks_extremes = []
+    trek_days = []
+    for gpx in gpxs:
+        if isinstance(gpx.days, np.ndarray):
+            trek_days.append(min(gpx.days))
+        else:
+            trek_days.append(gpx.days)
+        tracks.append(gpx.coordinate_raw)
+        tracks_ele.append(gpx.ele)
+        tracks_extremes.append([[gpx.coordinate_raw[gpx.times == min(gpx.times), 0],
+                                 gpx.coordinate_raw[gpx.times == min(gpx.times), 1]],
+                                [gpx.coordinate_raw[gpx.times == max(gpx.times), 0],
+                                 gpx.coordinate_raw[gpx.times == max(gpx.times), 1]]])
+
+    tracks_extremes = np.array(tracks_extremes)
+    trek_days = np.array(trek_days)
+
+    maxx = max([max(t[:, 0]) for t in tracks])
+    maxy = max([max(t[:, 1]) for t in tracks])
+
+    for t in tracks:
+        t[:, 0] = t[:, 0] / maxx
+        t[:, 1] = t[:, 1] / maxy
+
+    tracks_extremes[:, :, 0] = tracks_extremes[:, :, 0] / maxx
+    tracks_extremes[:, :, 1] = tracks_extremes[:, :, 1] / maxy
+
+    fig, ax = plt.subplots()
+    ax.tick_params(
+        axis="both",
+        which="both",
+        bottom=False,
+        left=False,
+        labelbottom=False,
+        labelleft=False
+    )
+
+    counter = 0
+    colmaps = ['viridis_r', 'plasma_r', 'inferno_r', 'magma_r', 'cividis_r']
+    for t, e in zip(tracks, tracks_ele):
+        ax.scatter(t[:, 1], t[:, 0], s=5, c=e, cmap=colmaps[counter % len(colmaps)])
+        counter += 1
+
+    counter = 0
+    for e in tracks_extremes:
+        ax.scatter(e[0, 1], e[0, 0], s=60, c='k') # start of the day
+        ax.scatter(e[1, 1], e[1, 0], s=60, c='k') # end of the day
+        # adding "notte numero " annotation
+        if counter != np.shape(tracks_extremes)[0]-1:    
+            ax.annotate("Notte " + str(counter + 1), xy=(e[1, 1], e[1, 0]), xytext=(30, 30),
+                        textcoords='offset points',
+                        color='k', size='large',
+                        arrowprops=dict(
+                        arrowstyle='simple,tail_width=0.2,head_width=0.8,head_length=0.8',
+                        color='k'))
+        counter += 1
+
+    ax.scatter(tracks_extremes[trek_days == max(trek_days), 1, 1], # end of the last day
+               tracks_extremes[trek_days == max(trek_days), 1, 0],
+               s=90, c='r')
+    ax.scatter(tracks_extremes[trek_days == min(trek_days), 0, 1], # start of the first day
+               tracks_extremes[trek_days == min(trek_days), 0, 0],
+               s=90, c='g')
+    fig.savefig(outpath + 'multidaytrack.pdf', bbox_inches="tight")
+    plt.close(fig)
+    return 0
+
+
+def plothr(gpx: tl.GpxReadout, outpath: str):
     if gpx.numhr != 0:
         fig, ax = plt.subplots()
         ax.xaxis.set_major_formatter(FuncFormatter(lambda x, pos: gpx.convert_elap(x)))
@@ -156,7 +138,7 @@ def plothr(gpx: GpxReadout, outpath: str):
         return -1
 
 
-def plotele(gpx: GpxReadout, outpath: str):
+def plotele(gpx: tl.GpxReadout, outpath: str):
     if gpx.numele != 0:
         fig, ax = plt.subplots()
         ax.xaxis.set_major_formatter(FuncFormatter(lambda x, pos: gpx.convert_elap(x)))
@@ -173,33 +155,44 @@ def plotele(gpx: GpxReadout, outpath: str):
 
 
 def main():
-    path = ''
-    outpath = ''
-    timestrings = []
 
-    # arguments parsing bruttissimo 
+    multiday = False
 
-    for argind in range(len(sys.argv)):
-        if sys.argv[argind] == "-p":
-            argind += 1
-            path = sys.argv[argind]
-        if sys.argv[argind] == "-op":
-            argind += 1
-            outpath = sys.argv[argind]
-        if sys.argv[argind] == "-t":
-            timestrings = []
-            argind += 1
-            while re.match("-[a-z]", sys.argv[argind]) is None:
-                timestrings.append(sys.argv[argind])
-                if(argind == len(sys.argv) - 1):
-                    break
-                else:
-                    argind += 1
+    # argument parsing guardabile con tanti controlli che ci sta
+    args = tl.parse_args(sys.argv)
+    # input path
+    if "p" in args:
+        if len(args["p"]) != 1:
+            multiday = True
+        path = args["p"]
+    else:
+        print("No input file")
+        return -1
 
-    gpx = GpxReadout(path)
-    plottrack(gpx, timestrings, outpath)
-    plothr(gpx, outpath)
-    plotele(gpx, outpath)
+    # output path
+    if "-op" in args:
+        if len(args["op"]) != 1:
+            print("Too many output paths")
+            return -1
+        outpath = args["op"]
+    else:
+        outpath = ''
+
+    # time strings
+    if "t" in args:
+        timestrings = args["t"]
+    else:
+        timestrings = []
+
+    # execution
+    if not multiday:
+        gpx = tl.GpxReadout(path[0])
+        plottrack(gpx, outpath, timeinput = timestrings)
+        plothr(gpx, outpath)
+        plotele(gpx, outpath)
+    else:
+        gpxs = [tl.GpxReadout(p) for p in path]
+        plotmultiday(gpxs, outpath)
 
 
 if __name__ == "__main__":
---
+++ src_mappe/tools.py
@@ -0,0 +1,115 @@
+import numpy as np
+import re
+import datetime
+import sys
+
+class GpxReadout:
+    def __init__(self, filepath: str):
+        self.coordinate_raw = []
+        self.coordinate = []
+        self.times = []
+        self.days = []
+        self.hr = []
+        self.ele = []
+        self.numpoints = 0
+        self.numhr = 0
+        self.numtime = 0
+        self.numele = 0
+        self.ismultiday = True
+        # readout of the gpx file
+        with open(filepath, "r") as file:
+
+            intopoints = False
+            for line in file:
+                if not intopoints:
+                    if re.match("\s*<trk>\s*", line) is not None:
+                        intopoints = True
+                else:
+                    testpoint = re.match("\s*<trkpt\slat=\"([0-9]{2}.[0-9]*)\"\slon=\"([0-9]{2}.[0-9]*)\">\s*",
+                                         line)
+                    if testpoint is not None:
+                        self.numpoints += 1
+                        self.coordinate_raw.append([float(testpoint.group(1)), float(testpoint.group(2))])
+                        if self.numpoints % 1000 == 0:
+                            sys.stdout.write(str(self.numpoints) + "\r")
+                            sys.stdout.flush()
+                        continue
+
+                    testtime = re.match("\s*<time>([0-9]{4}-[0-9]{2}-[0-9]{2})T([0-9]{2}\:[0-9]{2}:[0-9]{2})\.000Z</time>\s*",
+                                        line)
+                    if testtime is not None:
+                        self.numtime += 1
+                        if self.numtime != self.numpoints:
+                            for i in range(self.numpoints - self.numtime):
+                                self.times.append(datetime.time.fromisoformat('00:00:00'))
+                                self.days.append(datetime.datetime.fromisoformat('1970-01-01'))
+                            self.numtime = self.numpoints
+                        time_obj = datetime.time.fromisoformat(testtime.group(2))
+                        day_obj = datetime.datetime.fromisoformat(testtime.group(1))
+                        self.times.append(time_obj)
+                        self.days.append(day_obj)
+                        continue
+
+                    testhr = re.match("\s*<ns3:hr>([0-9]+)</ns3:hr>\s*",
+                                      line)
+                    if testhr is not None:
+                        self.numhr += 1
+                        if self.numhr != self.numpoints:
+                            for i in range(self.numpoints - self.numhr):
+                                self.hr.append(np.nan)
+                            self.numhr = self.numpoints
+                        self.hr.append(int(testhr.group(1)))
+                        continue
+
+                    testele = re.match("\s*<ele>([0-9]+)(\.[0-9]+)?</ele>\s*", line)
+                    if testele is not None:
+                        self.numele += 1
+                        if self.numele != self.numpoints:
+                            for i in range(self.numpoints - self.numele):
+                                self.ele.append(None)
+                            self.numele = self.numpoints
+                        self.ele.append(int(testele.group(1)))
+                                
+
+        self.coordinate_raw = np.array(self.coordinate_raw)
+        self.coordinate = np.zeros(np.shape(self.coordinate_raw))
+        self.coordinate[:, 0] = self.coordinate_raw[:, 0] / np.max(self.coordinate_raw[:, 0])
+        self.coordinate[:, 1] = self.coordinate_raw[:, 1] / np.max(self.coordinate_raw[:, 1])
+        self.times = np.array(self.times)
+        self.days = np.array(self.days)
+        if np.all(self.days == self.days[0]):
+            # if the day is always the same is enough to keep one daytime obj
+            self.days = self.days[0]
+            self.ismultiday = False
+
+        def tosec(x):
+            return (x.hour * 60 + x.minute) * 60 + x.second
+
+        tosec_vec = np.vectorize(tosec)
+
+        self.elap_time = tosec_vec(self.times) - tosec(min(self.times))
+        self.hr = np.array(self.hr)
+        self.ele = np.array(self.ele)
+
+    def convert_elap(self, timesec):
+        eth = np.floor(timesec / 3600).astype(int)
+        etm = np.floor((timesec - 3600 * eth) / 60).astype(int)
+        ets = (timesec - 3600 * eth - 60 * etm).astype(int)
+        return str(eth) + ":" + str(etm) + ":" + str(ets)
+
+
+def parse_args(arglist):
+    args = {}
+    key = ''
+    arglist = arglist[1:]
+    for i in arglist:
+        rexp = re.match('-([a-z]+)', i)
+        if rexp:
+            key = rexp.group(1)
+            args[key] = []
+        else:
+            if key == '':
+                print('errore nel parsing')
+                return -1
+            args[key].append(i)
+    return args
